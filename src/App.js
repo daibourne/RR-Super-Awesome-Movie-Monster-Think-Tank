0eaa56cb1bdaee90822e2259355c9796fb7b69fe
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-06-22 16:54:31 -0700

    Finished Travel Agent Activity

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css';
 import AboutUs from './components/AboutUs'
 import Packages from './components/Packages'
+import React, { Suspense } from 'react'
+
+const AboutUs = React.lazy(() => improt('./AboutUs'))
+const Packages = React.lazy(() => import('./Packages'))
 
 function App() {
+  const AboutUs = React.lazy(() => improt('./AboutUs'))
+  const Packages = React.lazy(() => import('./Packages'))
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <AboutUs />
-          <Packages />
+          <Suspense>
+            <AboutUs />
+            <Packages />
+          </Suspense>
       </div>
     </div>
   );
 }
+
+
 
 export default App;
